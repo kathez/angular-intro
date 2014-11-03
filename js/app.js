3ef4344e89d0b6a679488bb33dd25ffebe0c6262
katherine <zhuq3@uw.edu>
2014-11-03 12:15:09 -0800

    added angular and table for filtering and searching

--- js/app.js
+++ js/app.js
@@ -5,3 +5,24 @@
 
 "use strict";
 
+//create new angular module
+//returns new angular module object, and we call the module controller method
+angular.module('MovieApp', [])
+    .controller('MoviesController', function($scope) {
+        $scope.movies = movies; //set the scope.movies to the movies object from js file movie we set
+        $scope.searchString = '';
+        $scope.sortCol = 'rank';
+        $scope.sortReverse = false;
+
+        $scope.sortBy = function(colName) {
+            if ($scope.sortCol == colName) {
+                $scope.sortReverse = !$scope.sortReverse
+            }
+            else {
+                $scope.sortReverse = false;
+            }
+            $scope.sortCol = colName;
+
+        }
+    });
+
